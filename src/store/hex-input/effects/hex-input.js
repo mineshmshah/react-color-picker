919147e41cd3bda13b0b8d077387569323ba4483
Minesh <mineshmshah@hotmail.co.uk>
2019-03-01 10:55:01 +0000

    hex to alpha updated

--- src/store/hex-input/effects/hex-input.js
+++ src/store/hex-input/effects/hex-input.js
@@ -9,7 +9,7 @@ function * validateHexInput () {
   let inputValue = yield select(state => state.hexInput.hex_input)
   const colorHexValue = yield select(state => state.color.hex)
 
-  const valid  = /(^#{0,1}[0-9A-F]{6}$)|(^#{0,1}[0-9A-F]{3}$)/i.test(inputValue);
+  const valid  = /(^#{0,1}[0-9A-F]{6}$)|(^#{0,1}[0-9A-F]{3}$)|(^#{0,1}[0-9A-F]{8}$)|(^#{0,1}[0-9A-F]{4}$)/i.test(inputValue);
 
 
   if (valid) {
@@ -19,15 +19,28 @@ function * validateHexInput () {
     if (inputValue.length === 3)
       inputValue = inputValue.replace(/([0-9A-F])([0-9A-F])([0-9A-F])/i,'$1$1$2$2$3$3');
 
+    if (inputValue.length === 4)
+      inputValue = inputValue.replace(/([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])/i,'$1$1$2$2$3$3$4$4');
+
     const FinalRed = Number.parseInt(inputValue.substr(0, 2), 16);
     const FinalGreen = Number.parseInt(inputValue.substr(2, 2), 16);
     const FinalBlue = Number.parseInt(inputValue.substr(4, 2), 16);
+
+    const hexValue = inputValue.length === 8
+      ? Number.parseInt(inputValue.substr(6, 2), 16)
+      :'255';
+    const newAlpha = (hexValue / 255).toFixed(2);
+
     const newRGB = { FinalRed, FinalGreen, FinalBlue };
     const newHSL = RGBtoHSL(FinalRed, FinalGreen, FinalBlue)
     const newHSV = RGBtoHSV(FinalRed, FinalGreen, FinalBlue)
     yield put({
       type: colorTypes.UPDATE_HEX,
       value: `#${inputValue}`
+    });
+    yield put({
+      type: colorTypes.UPDATE_A,
+      value: newAlpha
     });
     yield put({
       type:colorTypes.UPDATE_RGB_COMBO,
