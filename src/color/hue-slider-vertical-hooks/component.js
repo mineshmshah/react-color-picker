b6ed75d7292da35a08c35ef834b305e822463f97
Minesh <mineshmshah@hotmail.co.uk>
2019-03-04 16:04:30 +0000

    refactored position to use hooks

--- src/color/hue-slider-vertical-hooks/component.js
+++ src/color/hue-slider-vertical-hooks/component.js
@@ -1,58 +1,42 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import store from './enhancer/connect'
 import { HueSliderComponent , SliderBox, PickerSlider } from "./styles";
 
-class HueSliderVerticalHooks extends Component {
+const HueSliderVerticalHooks = ({h, areaHeight, hueSliderVerticalHooksActions}) => {
+  const [position, setPosition] = useState(0);
 
-  constructor(props){
-    super(props)
-    this.updateHueSlider=this.updateHueSlider.bind(this);
-    this.mouseDownEvent = this.mouseDownEvent.bind(this)
-  }
+  useEffect(()=> {
+    const newPosition = (h * areaHeight) /360;
+    setPosition(newPosition)
+  },
+  [h, areaHeight]
+  );
 
-  componentDidMount(){
-    this.updateHueSlider()
-  }
-
-  componentDidUpdate(){
-    this.updateHueSlider()
-  }
-
-  updateHueSlider() {
-    const {hueSliderVerticalHooksActions, h, areaHeight} = this.props;
-    hueSliderVerticalHooksActions.updateHueSliderPositionVerticalHooks(h,areaHeight)
-  }
-
-  updateHValueWithSlider(e, sliderAreaOffset){
-    const {areaHeight, hueSliderVerticalHooksActions} = this.props;
+  const updateHValueWithSlider = (e, sliderAreaOffset) => {
     let yValue =e.pageY - sliderAreaOffset;
     if (yValue > areaHeight) yValue = areaHeight;
     if (yValue < 0) yValue = 0;
     const hue = Math.round((yValue/areaHeight) * 359);
     hueSliderVerticalHooksActions.updateColorsWithHueSliderVerticalHooks(hue);
-  }
+  };
 
-  mouseDownEvent(e){
+  const mouseDownEvent = e => {
     const sliderAreaOffset = e.currentTarget.offsetTop;
-    this.updateHValueWithSlider(e, sliderAreaOffset);
-    const HUpdaterFunction = event => this.updateHValueWithSlider(event, sliderAreaOffset);
+    updateHValueWithSlider(e, sliderAreaOffset);
+    const HUpdaterFunction = event => updateHValueWithSlider(event, sliderAreaOffset);
     document.addEventListener('mousemove',  HUpdaterFunction) ;
     document.addEventListener('mouseup', () =>
       document.removeEventListener('mousemove', HUpdaterFunction))
-  }
-
-  render() {
-    const {position, areaHeight} = this.props;
-    return(
-      <HueSliderComponent>
-        <SliderBox areaHeight={areaHeight} onMouseDown={e=>this.mouseDownEvent(e)} >
-          <PickerSlider sliderY={position}/>
-        </SliderBox>
-      </HueSliderComponent>
-    )
-  }
-}
+  };
+  return(
+    <HueSliderComponent>
+      <SliderBox areaHeight={areaHeight} onMouseDown={e=> mouseDownEvent(e)} >
+        <PickerSlider sliderY={position}/>
+      </SliderBox>
+    </HueSliderComponent>
+  )
+};
 
 export default store(HueSliderVerticalHooks);
 
@@ -61,12 +45,10 @@ HueSliderVerticalHooks.propTypes = {
   hueSliderVerticalHooksActions: PropTypes.objectOf(PropTypes.func),
   h: PropTypes.number,
   areaHeight:PropTypes.number,
-  position:PropTypes.number,
 };
 
 HueSliderVerticalHooks.defaultProps = {
   hueSliderVerticalHooksActions: {},
   h: 0,
   areaHeight:160,
-  position:0,
 };
