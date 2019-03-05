1715dad5b496b699e8e81992f3610469631002f7
Minesh <mineshmshah@hotmail.co.uk>
2019-03-05 16:42:42 +0000

    add build

---
+++ build/precache-manifest.321ea555a9d3037ff70d745ba8d4be5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bd23a9fa216a543b3050",
+    "url": "/static/js/main.bd23a9fa.chunk.js"
+  },
+  {
+    "revision": "27466e9e2c8398bee311",
+    "url": "/static/js/2.27466e9e.chunk.js"
+  },
+  {
+    "revision": "bd23a9fa216a543b3050",
+    "url": "/static/css/main.7d66ca1c.chunk.css"
+  },
+  {
+    "revision": "2838faed5dda951ffacfe02e03fe3321",
+    "url": "/index.html"
+  }
+];
