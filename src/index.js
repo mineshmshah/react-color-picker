16d2b58c919b9df5b001fd1d614cd5a37eea52c4
Minesh <mineshmshah@hotmail.co.uk>
2019-03-07 11:53:18 +0000

    Removed state from index

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ render(
   <div>
     <App color='#FFF' onChange={({hex})=>console.log('App1', hex)}/>
     <App color='#4458' onChange={({hex})=>console.log('App2', hex)}/>
-    <div>
-      <button onClick={()=>this.setState({button1: !this.state.button})}>Button 1</button>
-    </div>
-    { this.state.button1 &&  <App color='#CCA365' onChange={({hex})=>console.log('App3', hex)} />}
   </div>
   ,
   document.getElementById('root')
