14d916d6d2982062e0d69cce2c32dc29bd9e0df4
Minesh <mineshmshah@hotmail.co.uk>
2019-03-07 09:48:13 +0000

    testing adding new component

--- src/AppWithProvider.js
+++ src/AppWithProvider.js
@@ -3,11 +3,32 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 import App from './App';
 
-const AppWithProvider = () => (
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>
-);
+class AppWithProvider extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      button1 : false
+    }
+  }
 
+  render() {
+    return(
+      <div>
+        <Provider store={configureStore()}>
+          <App />
+        </Provider>
+        <Provider store={configureStore()}>
+          <App />
+        </Provider>
+        <div>
+          <button onClick={()=>this.setState({button1: !this.state.button})}>Button 1</button>
+        </div>
+        { this.state.button1 &&  <Provider store={configureStore()}>
+          <App />
+        </Provider>}
+      </div>
+    )
+  }
+}
 
 export default AppWithProvider
